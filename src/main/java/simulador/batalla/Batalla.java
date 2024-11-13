91e6765840ec18de00a9ff6b46341392f9e6acd5
Sebastian <sebasmolmu.2007@gmail.com>
2024-11-12 23:10:54 -0500

    Se agrega clase Batalla, con el metodo de iniciarBatalla para que se gestionen las batallas entre dos pokemones

--- src/main/java/simulador/batalla/Batalla.java
+++ src/main/java/simulador/batalla/Batalla.java
@@ -1,5 +1,35 @@
 package simulador.batalla;
 
+
+
 public class Batalla {
-    // TODO: Aquí va tu código
+    
+    public void iniciarBatalla (Pokemon pokemon1, Pokemon pokemon2){
+        System.out.println("¡Empieza la batalla entre "+pokemon1.getNombre()+" y "+pokemon2.getNombre()+"!");
+    
+        while(pokemon1.getSalud()>0 && pokemon2.getSalud()>0 ){
+        ejecutarTurno(pokemon1, pokemon2);
+        if(pokemon2.getSalud()>0){
+        ejecutarTurno(pokemon2, pokemon1);
+        }
+        }
+        if(pokemon1.getSalud()>0){
+            System.out.println(pokemon1.getNombre()+" ha ganado la batalla");
+        }
+        else{
+            System.out.println(pokemon2.getNombre()+ " ha ganado la batalla");
+        }
+    }
+    
+    private void ejecutarTurno (Pokemon atacante, Pokemon defensor){
+        System.out.println(atacante.getNombre()+" ataca a "+defensor.getNombre());
+        
+        double multiplicador = TipoPokemon.obtenerMultiplicadorDeDaño(atacante.getTipo(), defensor.getTipo());
+        int daño = (int)(atacante.getPuntosDeAtaque()*multiplicador);
+        
+        defensor.recibirDaño(daño);
+        
+        System.out.println(defensor.getNombre()+" ha recibido "+ daño + " puntos de daño");
+        System.out.println("Salud restante: "+defensor.getSalud());
+    }
 }

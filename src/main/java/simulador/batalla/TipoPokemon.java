ed0618fc3f2834dd9c9e2f8ad4ba18c6a4d17909
david.cardenas <david.cardenas0718@gmail.com>
2024-11-19 19:38:29 -0500

    se Define la enumeración TipoPokemon con los tipos principales y el método obtenerMultiplicadorDeDaño para calcular ventajas de tipo. Agrega placeholders para nuevos tipos como BICHO, HIELO, ACERO y HADA

--- src/main/java/simulador/batalla/TipoPokemon.java
+++ src/main/java/simulador/batalla/TipoPokemon.java
@@ -1,5 +1,13 @@
-package simulador.pokemon;
+package simulador.batalla;
 
-public class TipoPokemon {
-    // TODO: Aquí va tu código
+
+public enum TipoPokemon {
+    FUEGO, AGUA, PLANTA, VENENO, ELECTRICO, PSÍQUICO, ROCA, TIERRA, VOLADOR, NORMAL, LUCHA;
+
+    public static double obtenerMultiplicadorDeDaño(TipoPokemon atacante, TipoPokemon defensor) {
+        TipoPokemon BICHO = null;
+        TipoPokemon HIELO = null;
+        TipoPokemon ACERO = null;
+        TipoPokemon HADA = null; 
+
 }

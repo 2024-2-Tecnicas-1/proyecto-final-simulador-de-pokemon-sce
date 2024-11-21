4e611b5dd7a4ea6a68e64e3168e28159c098a0df
SebasC22 <sebastian22campo@gmail.com>
2024-11-20 23:36:18 -0500

    Se agrega la clase Bellsprout con sus atributos

---
+++ src/main/java/simulador/batalla/Bellsprout.java
@@ -0,0 +1,18 @@
+
+package simulador.batalla;
+
+
+public class Bellsprout {
+public class Bellsprout extends Pokemon {
+    public Bellsprout(String nombre) {
+        super(nombre, 50, 75, TipoPokemon.PLANTA);
+    }
+
+    @Override
+    public void atacar(Pokemon oponente) {
+        double multiplicador = TipoPokemon.obtenerMultiplicadorDeDaño(this.getTipo(), oponente.getTipo());
+        int daño = (int) (this.getPuntosDeAtaque() * multiplicador);
+        oponente.recibirDaño(daño);
+    }
+}
+}

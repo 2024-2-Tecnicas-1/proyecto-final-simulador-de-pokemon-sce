a4932c2e45e349d27fe6ce31b8a8a80984d504db
SebasC22 <sebastian22campo@gmail.com>
2024-11-20 23:48:06 -0500

    Se agrega la clase Exeggcute

---
+++ src/main/java/simulador/batalla/Exeggcute.java
@@ -0,0 +1,18 @@
+
+package simulador.batalla;
+
+public class Exeggcute {
+    public class Exeggcute extends Pokemon {
+    public Exeggcute(String nombre) {
+        super(nombre, 60, 40, TipoPokemon.PLANTA);
+    }
+
+    @Override
+    public void atacar(Pokemon oponente) {
+        double multiplicador = TipoPokemon.obtenerMultiplicadorDeDaño(this.getTipo(), oponente.getTipo());
+        int daño = (int) (this.getPuntosDeAtaque() * multiplicador);
+        oponente.recibirDaño(daño);
+    }
+}
+
+}
